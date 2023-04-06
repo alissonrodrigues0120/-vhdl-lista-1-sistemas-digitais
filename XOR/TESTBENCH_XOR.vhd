0c2d2e8f91b4ea983ebb058c51c1b0d572765c86
Álisson Rodrigues Fernandes <97190682+alissonrodrigues0120@users.noreply.github.com>
2023-04-06 14:30:41 -0300

    Create TESTBENCH_XOR.vhd

---
+++ XOR/TESTBENCH_XOR.vhd
@@ -0,0 +1,49 @@
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity TESTBENCH_XOR is
+   
+end TESTBENCH_XOR;
+
+architecture Behavioral of TESTBENCH_XOR is
+  component XOR_EDIT
+      Port ( A : in STD_LOGIC;
+           B : in STD_LOGIC;
+           X : out STD_LOGIC);
+           
+  end component;
+   signal A, B, X: std_logic;
+   constant tempinho : time := 100 ns;
+   
+
+begin
+  DUT : XOR_EDIT port map (A=>A, B=>B, X=>X);
+  
+  processando: process
+  begin
+    A <= '0';
+    B <= '0';
+    
+    wait for tempinho;
+    
+    A <= '0';
+    B <= '1';
+    
+    wait for tempinho;
+    
+    A <= '1';
+    B <= '0';
+    
+    wait for tempinho;
+    
+    A <= '1';
+    B <= '1';
+    
+    wait for tempinho;
+
+  end process;
+
+end Behavioral;
